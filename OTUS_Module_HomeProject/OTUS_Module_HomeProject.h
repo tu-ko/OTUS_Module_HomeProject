a11f6f842840ec71818b2b54faf1af524706dc0b
Konstantin Tukmakov <tukmakov.konstantin@gmail.com>
2022-12-22 19:24:56 +0300

    Init

---
+++ OTUS_Module_HomeProject/OTUS_Module_HomeProject.h
@@ -0,0 +1,18 @@
+//
+//  OTUS_Module_HomeProject.h
+//  OTUS_Module_HomeProject
+//
+//  Created by Konstantin Tukmakov on 22.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OTUS_Module_HomeProject.
+FOUNDATION_EXPORT double OTUS_Module_HomeProjectVersionNumber;
+
+//! Project version string for OTUS_Module_HomeProject.
+FOUNDATION_EXPORT const unsigned char OTUS_Module_HomeProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTUS_Module_HomeProject/PublicHeader.h>
+
+
